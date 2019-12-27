dc4b6265eb809f0b583996da9ca59b5b5aff0879
pavTiger <pavTiger@gmail.com>
2019-12-27 12:29:41 +0300

    tesst

---
+++ main.py
@@ -0,0 +1,33 @@
+import datetime
+import requests
+import vk_api
+from bs4 import BeautifulSoup
+from random import randint
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.longpoll import VkLongPoll, VkEventType
+vk_session = vk_api.VkApi(token='3ae03ce89d13effca33ecac09e6919dd03eb1b4dc4b55fe33075c16ae6ad90f4b58fcb724b2b6fc07826b')
+longpoll = VkLongPoll(vk_session)
+
+STATE = 'idle'
+
+user_old = dict()
+def send(mess):
+    vk.messages.send(
+        user_id=event.user_id,
+        keyboard=StartKeyboard.get_keyboard(),
+        message=mess, random_id=randint(0, 2147483647))
+
+
+vk = vk_session.get_api()
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+        event.text = event.text.lower()
+
+        if event.text == 'start' or event.text == 'начать':
+                StartKeyboard = VkKeyboard(one_time=True)
+                StartKeyboard.add_button('Добавить форму', color=VkKeyboardColor.DEFAULT)
+                send('Привет''&#128075;')
+                STATE = 
+        
+        else:
+            send('ERROR')
